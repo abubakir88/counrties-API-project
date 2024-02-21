4c975315cea03bdffc3157325365c6c0422e1017
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-02-22 03:23:09 +0500

    Add Modal

--- JS/index.js
+++ JS/index.js
@@ -10,7 +10,7 @@ async function getCountry() {
   const url = await fetch("./index.json");
   const res = await url.json();
   const resdata = res.data;
-  //   console.log(resdata);
+  console.log(resdata);
   resdata.forEach((element) => {
     showCountry(element);
   });
@@ -26,24 +26,25 @@ function showCountry(data) {
 </div>
 <div class="country-info">
   <h5 class="countryName">${data.name.common}</h5>
-  <p><b>Population</b> ${data.population}</p>
+  <p><b>Population:</b> ${data.population}</p>
   <p class="regionName"><b>Region:</b> ${data.region}</p>
-  <p><b>Capital</b> ${data.capital}</p>
+  <p><b>Capital:</b> ${data.capital}</p>
   </div>`;
   countriesElem.appendChild(country);
+  country.addEventListener("click", () => {
+    showCountryDetail(data);
+  });
 }
 
 dropDown.addEventListener("click", () => {
   dropElem.classList.toggle("showDropDown");
-  console.log("hello");
 });
 
 const regionName = document.getElementsByClassName("regionName");
 const countryName = document.getElementsByClassName("countryName");
 region.forEach((element) => {
   element.addEventListener("click", () => {
-    console.log(element);
-    element.parentElement.style.display = "none"; /////////
+    // console.log(element);
     Array.from(regionName).forEach((elem) => {
       if (
         elem.innerText.includes(element.innerText) ||
@@ -56,8 +57,7 @@ region.forEach((element) => {
 });
 
 search.addEventListener("input", () => {
-  //   console.log(search.value);
-  console.log(search.value.toLowerCase());
+  // console.log(search.value.toLowerCase());
   Array.from(countryName).forEach((elem) => {
     if (elem.innerText.toLowerCase().includes(search.value.toLowerCase())) {
       elem.parentElement.parentElement.style.display = "grid";
@@ -65,7 +65,61 @@ search.addEventListener("input", () => {
   });
 });
 
+// DARK-MODE///////////////////
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  // moon.classList.toggle("fas");
 });
+
+//MODAL///////////////////////////////////
+const counrtyModal = document.querySelector(".counrtyModal");
+
+function showCountryDetail(data) {
+  // function getCurrenciesName(country) {
+  //   const currenciesKey = Object.keys(country.currencies)[0];
+  //   const currenciesName = country.currencies[currenciesKey].name;
+  //   return currenciesName;
+  // }
+  counrtyModal.classList.toggle("show");
+  let [, languages] = Object.keys(data.languages);
+  let currencies = Object.keys(data.currencies)[0];
+  console.log(currencies);
+  counrtyModal.innerHTML = `<button class="back">
+  <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+        >
+          <path
+            d="M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"
+          ></path>
+        </svg>
+          Back</button>
+  <div class="modal">
+    <div class="leftModal">
+      <img src="${data.flags.png}" alt="" />
+    </div>
+    <div class="rightModal">
+      <h1>${data.name.common}</h1>
+      <div class="moldalInfo">
+        <div class="innerLeft inner">
+          <p><b>Native Name: </b> ${data.altSpellings[2]}</p>
+          <p><b>Population: </b> ${data.population}</p>
+          <p><b>Region: </b> ${data.region}</p>
+          <p><b>Sub-region: </b> ${data.subregion}</p>
+        </div>
+        <div class="innerRight inner">
+          <p><b>Capital: </b> ${data.capital}</p>
+          <p><b>Top Level Domain: </b> ${data.tld}</p>
+          <p><b>Currencies: </b> ${currencies}</p>
+          <p><b>Languages: </b> ${languages}</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  const back = counrtyModal.querySelector(".back");
+  back.addEventListener("click", () => {
+    counrtyModal.classList.toggle("show");
+  });
+}
